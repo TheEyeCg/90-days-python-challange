b1a547e587dd51ff382cbcf076b1fe4e1f3799fe
TheEyeCg <cybergirl913@gmail.com>
2025-01-06 17:11:41 +0100

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+print ("Hello World!")
+
+#basic arithemetic
+
+a=2
+b=3
+
+#addition
+print (f"{a} + {b} ={a+b}")
+
+#subtraction
+print (f"{a} - {b} ={a-b}")
+
+#multiplication
+print (f"{a} * {b} ={a*b}")
+
+#division
+print ( f"{a} / {b} ={a/b}")
